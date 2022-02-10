cc2b921f5b2f22e44fcdefaedab69960a5ebc0ca
naitikvarshney <69815982+naitikvarshney@users.noreply.github.com>
2022-02-10 23:04:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://inshareapp-project.herokuapp.com/";
+const baseURL =  "http://localhost:3000/";  //"https://inshareapp-project.herokuapp.com/";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
